f9a6494673a1e0571d9a5f4a487033aba700a3db
Labfox <labfoxdev@gmail.com>
2025-07-20 16:19:48 +0000

    Licensing 2

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+# Whatsapp Audio Transcriber - A simple bot to transcribe vocal messages
+# Copyright (C) 2025 Labfox
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from whatsfly import WhatsApp
 import time
 import whisper
